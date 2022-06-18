259be5167c94bfb17af9703bfe815d959788a6d8
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 21:08:22 +0530

    Update Decending_Array.java

--- Decending_Array.java
+++ Decending_Array.java
@@ -1,20 +1,25 @@
 import java.util.*;
 class Decending_Array
 {
+    // decending method calling by the inside the main method
     public static void decending()
     {
         
         int[] arr = new int[10];
         int temp=0;
         
+        // taking 10 values from user
         System.out.print("Please enter the 10 values : ");
         
+        // taking array from user
         for(int i=0; i<arr.length;i++)
         {
             Scanner sc = new Scanner(System.in);
             arr[i] = sc.nextInt();
             
         }
+        
+        // array sorting in the loop
         for(int i=0; i<arr.length; i++)
         {
             for(int j=i+1; j<arr.length; j++)
@@ -27,10 +32,11 @@ class Decending_Array
                 }
             }
         }
-
+        
+        // breaking the line
         System.out.println();
 
-
+        // after sorting array , Array will be print
         System.out.println("Elements of array sorted in decending order is : ");    
         for (int i = 0; i < arr.length; i++)
          {     
@@ -39,9 +45,11 @@ class Decending_Array
          }  
     }    
       
-     
+    
+    // main method 
      public static void main(String[] args)
      {
+         // decending method is calling
         decending();
     }
  }
