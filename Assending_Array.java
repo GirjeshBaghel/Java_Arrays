9cb6905a0bac764ef09f1c75bd2e1a71c4614084
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-18 20:59:03 +0530

    Update Assending_Array.java

--- Assending_Array.java
+++ Assending_Array.java
@@ -1,20 +1,24 @@
 import java.util.*;
 class Assending_Array
 {
+    // it is assending function that call by main method
     public static void assending()
     {
         
         int[] arr = new int[10];
         int temp=0;
-        
+                
         System.out.print("Please enter the 10 values : ");
         
+        //taking array from user 
         for(int i=0; i<arr.length;i++)
         {
             Scanner sc = new Scanner(System.in);
             arr[i] = sc.nextInt();
             
         }
+        
+        // sorting array in assending array
         for(int i=0; i<arr.length; i++)
         {
             for(int j=i+1; j<arr.length; j++)
@@ -30,7 +34,7 @@ class Assending_Array
 
         System.out.println();
 
-
+        // after sort a array , Array will be print
         System.out.println("Elements of array sorted in ascending order: ");    
         for (int i = 0; i < arr.length; i++)
          {     
@@ -39,9 +43,10 @@ class Assending_Array
          }  
     }    
       
-     
+     // main method
      public static void main(String[] args)
      {
+         // assending function is calling
         assending();
     }
  }
