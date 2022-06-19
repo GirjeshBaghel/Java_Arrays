80510ff6c4b182d36bd9e23e0e1bed1dd2b93890
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 10:31:46 +0530

    Update Smallest_Array.java

--- Smallest_Array.java
+++ Smallest_Array.java
@@ -2,6 +2,8 @@ import java.util.*;
 class Smallest_Array
 {
     int n;
+    
+    // this is smallest function
     public static int Smallest()
     {
         
@@ -9,25 +11,31 @@ class Smallest_Array
         
         System.out.print("Please enter the 10 values : ");
         
+        
+        // taking array from user
         for(int i=0; i<arr.length;i++)
         {
             Scanner sc = new Scanner(System.in);
             arr[i] = sc.nextInt();
         }
         
+        int small = arr[0];
         
-        int max = arr[0];
-         for (int i = 1; i < arr.length; i++)
+        // checking the condition which is smallest value       
+        for (int i = 1; i < arr.length; i++)
          {
-            if (arr[i] < max)
-            max = arr[i];
+            if (arr[i] < small)
+            small = arr[i];
          }
-         return max;
+        
+        // return the final output to the method
+         return small;
      }
       
-     
+     // main method
      public static void main(String[] args)
      {
+         // Smallest method is calling and taking output from the return type
          System.out.println("Smallest in given array is " + Smallest());
     }
  }
