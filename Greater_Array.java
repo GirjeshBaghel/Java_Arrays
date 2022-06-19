31332881fbe997ace4f248c04a0a1abdfed0a8d0
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-06-19 10:26:05 +0530

    Update Greater_Array.java

--- Greater_Array.java
+++ Greater_Array.java
@@ -2,6 +2,8 @@ import java.util.*;
 class Greater_Array
 {
     int n;
+    
+    // largest function that call inside the main method
     public static int largest()
     {
         
@@ -9,25 +11,30 @@ class Greater_Array
         
         System.out.print("Please enter the 10 values : ");
         
+        // taking array from user
         for(int i=0; i<arr.length;i++)
         {
             Scanner sc = new Scanner(System.in);
             arr[i] = sc.nextInt();
         }
         
-        
+        // passing array index value in max to compare the variable
         int max = arr[0];
          for (int i = 1; i < arr.length; i++)
          {
                 if (arr[i] > max)
                  max = arr[i];
          }
+        
+        // return the maximum value to largest function
          return max;
      }
       
-     
+     // main method
+    
      public static void main(String[] args)
      {
+         // In this line largest function is calling and taking the final output from the return type
          System.out.println("Largest in given array is " + largest());
     }
  }
