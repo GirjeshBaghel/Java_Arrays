793b28fc7f5ce1fd1dfba50100c95fe8afe34a8a
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-21 21:12:24 +0530

    Update Multiply_2DArray.java

--- Multiply_2DArray.java
+++ Multiply_2DArray.java
@@ -1,54 +1,55 @@
+// taking 2D two array from user than multiplication of that 2d array
 package Arrays;
 import java.util.Scanner;
 
 public class Mulitple_2D {
 	
-	  Scanner sc = new Scanner(System.in);
+	  Scanner sc = new Scanner(System.in);					// global variable declaration
 	 int row,col,i,j,k;
 	
-	public  void multiple()
+	public  void multiple()							// multiple method
 	{
 		
-		System.out.print("Enter the Row : ");
+		System.out.print("Enter the Row : ");				// taking no of row from user
 		row = sc.nextInt();
-		System.out.print("Enter the Col : ");
+		System.out.print("Enter the Col : ");				// taking no of col from user
 		col = sc.nextInt();	
 		
-		if(row == col)
+		if(row == col)							// if no of row are equal to no of col then process will be execute
 		{
 		
-			System.out.println("A Array : ");
+			System.out.println("A Array : ");			// taking first from user
 			int arr[][] = new int[row][col];
 			for(i=0; i<row; i++)
 			{
 				for(j=0; j<col; j++)
 				{
-					arr[i][j] = sc.nextInt();
+					arr[i][j] = sc.nextInt();		// storing that array in a loop format
 					
 				}
-				System.out.println();
+				System.out.println();				// breaking the line
 			}
 			
-			System.out.println("B Array : ");
+			System.out.println("B Array : ");			// taking b array from user
 			int brr[][] = new int[row][col];
 			for(i=0; i<row; i++)
 			{
 				for(j=0; j<col; j++)
 				{
-					brr[i][j] = sc.nextInt();
+					brr[i][j] = sc.nextInt();		// storing that array from the loop
 					
 				}
 				System.out.println();
 			}
-			int crr[][] = new int[row][col];
-						System.out.println("Multiplication of these Matrix is...");
+			int crr[][] = new int[row][col];			// taking a another array to strong both array
+			System.out.println("Multiplication of these Matrix is...");
 				for(i=0; i<row; i++)
 				{
 					for(j=0; j<col; j++)
 					{
 						for(k=0; k<arr.length; k++)
 						{
-							crr[i][j]+= arr[i][k]*brr[j][k]; 
+							crr[i][j]+= arr[i][k]*brr[j][k];  // mulitiplication of both array by the help of third array
 						}
 					}
 				}
@@ -57,7 +58,7 @@ public class Mulitple_2D {
 				{
 					for(j=0; j<col; j++)
 					{
-						System.out.print("   "+crr[i][j]);
+						System.out.print("   "+crr[i][j]);	// after multiplication array will be print
 						
 					}
 					System.out.println();
@@ -65,14 +66,14 @@ public class Mulitple_2D {
 			
 		}
 		else
-			System.out.print("Please write Square matrix here..");
+			System.out.print("Please write Square matrix here..");		// if row and col are not equal then else part will be execute
 		
 	}
-	
-	public static void main(String[] args)
+					
+	public static void main(String[] args)						// main method
 	{
-		Mulitple_2D ob = new Mulitple_2D();
-		ob.multiple();
+		Mulitple_2D ob = new Mulitple_2D();					// creating object of class
+		ob.multiple();								// mulitple method is calling
 		
 	}
 
